8429a71d983b7a240fbe1deefb4fb8fd443fd302
Maleina Bidek <maleina@ucla.edu>
2020-02-14 09:19:54 -0500

    fix: Remove unused files from index.html

--- src/server/index.js
+++ src/server/index.js
@@ -1,9 +1,21 @@
+const dotenv = require('dotenv');
+dotenv.config();
 const path = require('path')
 const express = require('express')
 const mockAPIResponse = require('./mockAPI.js')
 const aylien = require("aylien_textapi");
 
 const app = express()
+
+/* Middleware*/
+//Here we are configuring express to use body-parser as middle-ware.
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Cors for cross origin allowance
+const cors = require('cors');
+app.use(cors());
 
 app.use(express.static('dist'))
 
