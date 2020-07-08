15cc288f977959afa35915ef21f70326b76fdc3b
Maleina Bidek <maleina@ucla.edu>
2020-07-08 13:57:14 -0400

    refactor: Clean up and reformat up code

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,8 +1,6 @@
 function handleSubmit(event) {
     event.preventDefault();
 
-
-    // check what text was put into the form field
     let formText = document.getElementById('url').value;
 
     if (Client.checkURL(formText)){
